89f74310e2b1985477d170e629e88ef9da585172
Etterke <etterke@gmail.com>
2023-11-14 09:31:14 +0100

    added chart.js and sample pie chart

--- iata-dashboard-app/src/app/components/sales-analytics/sales-analytics.component.ts
+++ iata-dashboard-app/src/app/components/sales-analytics/sales-analytics.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChartOptions } from 'chart.js';
+import { NgChartsModule } from 'ng2-charts';
 
 @Component({
   selector: 'app-sales-analytics',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgChartsModule],
   templateUrl: './sales-analytics.component.html',
   styleUrl: './sales-analytics.component.scss'
 })
-export class SalesAnalyticsComponent {}
+export class SalesAnalyticsComponent {
+  public pieChartOptions: ChartOptions<'pie'> = {
+    responsive: false
+  };
+  public pieChartLabels = [
+    ['Download', 'Sales'],
+    ['In', 'Store', 'Sales'],
+    'Mail Sales'
+  ];
+  public pieChartDatasets = [
+    {
+      data: [300, 500, 100]
+    }
+  ];
+  public pieChartLegend = true;
+  public pieChartPlugins = [];
+}
